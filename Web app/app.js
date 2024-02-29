e8b2b2de6c2cc0fdeae957b18c1635c9c6a2b679
Jake Dorick <jdorick8@gmail.com>
2024-02-29 17:17:02 -0500

    Fixed dragover effect

--- Web app/app.js
+++ Web app/app.js
@@ -1,28 +1,3 @@
-// let uploadedFilePath; // Global variable to store the uploaded image path
-
-// function handleUpload() {
-//     displayLoadingBar();
-//     const fileInput = document.getElementById('uploadInput');
-//     const files = fileInput.files;
-
-//     if (files.length > 0) {
-//         uploadedFilePath = URL.createObjectURL(files[0]); // Store the uploaded image path
-//         simulateProcessing(files, 0);
-//     }
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Retrieve the uploaded image path from the query parameter
-//     const uploadedFilePath = getParameterByName('uploadedFilePath');
-
-//     // Update the 'resultImage' element with the uploaded image
-//     const resultImage = document.getElementById('resultImage');
-
-//     if (resultImage) {
-//         resultImage.src = uploadedFilePath;
-//     }
-// });
-
 function displayLoadingBar() {
     const loadingBar = document.getElementById('loadingBar');
     loadingBar.style.width = '0%';
@@ -65,33 +40,3 @@ function simulateProcessing(files, index) {
         }, 2000); // Simulated processing time (adjust as needed)
     }
 }
-
-// function showResultsButton() {
-//     const resultsButton = document.createElement('button');
-//     resultsButton.textContent = 'See Results';
-//     resultsButton.id = 'resultsButton';
-//     resultsButton.addEventListener('click', redirectToResults);
-
-//     // Pass the uploaded image path as a query parameter
-//     resultsButton.dataset.uploadedFilePath = uploadedFilePath;
-
-//     const container = document.getElementById('container');
-//     container.appendChild(resultsButton);
-
-//     // Apply CSS styles for the fade-in effect
-//     resultsButton.style.opacity = '0';
-//     resultsButton.style.transition = 'opacity 0.5s';
-
-//     // Use setTimeout to delay the appearance for the fade-in effect
-//     setTimeout(() => {
-//         resultsButton.style.opacity = '1';
-//     }, 100);
-// }
-
-// function redirectToResults() {
-//     // Retrieve the uploaded image path from the data attribute
-//     const uploadedFilePath = document.getElementById('resultsButton').dataset.uploadedFilePath;
-
-//     // Redirect to results.html with the uploaded image path as a query parameter
-//     window.location.href = `results.html?uploadedFilePath=${encodeURIComponent(uploadedFilePath)}`;
-// }
